a56152454a1b1002f5fca2b6f226fc6b03539521
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-21 22:23:15 +0530

    Add files via upload

---
+++ 2 dimensional arry.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[3][3],i,j;
+    clrscr();
+    gotoxy(40,13);
+    for(i=0;i<3;i++)
+    {
+    for(j=0;j<3;j++)
+    {
+    printf("enter a[%d][%d]",i,j);
+    scanf("%d",&a[i][j]);
+    }
+    }
+    printf("parint elements.....\n");
+    for(i=0;i<3;i++)
+    {
+    printf("\n");
+    for(j=0;j<3;j++)
+    {
+    printf("%d\t",a[i][j]);
+}
+}
+    getch();
+}
