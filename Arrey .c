b81fa94c5f36d0ad410241082e64f8187f532eb7
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-21 22:20:08 +0530

    Initial commite

---
+++ Arrey .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdio.h>
+int main(){
+      int r[5]={40,50,60,70,80},i;
+      clrscr();
+      printf("\n mark details");
+    for(i=0;i<5;i++)
+    {
+    printf("\nr%d=%d\n",i+1,r[i]);
+    }
+    getchar();
+    
+ }
+     
